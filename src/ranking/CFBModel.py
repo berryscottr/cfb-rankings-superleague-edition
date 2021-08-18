27a6a8cadc056418ca059982e0ee0aa1be0f7a94
ChangedNameTo <will.n.mitchell@gmail.com>
2021-08-18 15:05:20 -0400

    Added in xgboosting and filled nans

--- src/ranking/CFBModel.py
+++ src/ranking/CFBModel.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-
+import xgboost as xgb
 
 class CFBModel:
     def __init__(self, df):
@@ -23,21 +23,31 @@ class CFBModel:
         return prediction
 
     def spread_prediction(self):
-        regression_model = LinearRegression()
         game_data = self.data['cleaned_games']
 
         # Select columns
-        x = game_data.loc[:,['season', 'week', 'season_type', 'neutral_site', 'conference_game', 'venue_id', 'team_id',  'team_conference', 'team_wins','team_loses','games_played','season_point_total','team_line_score_q1_total','team_line_score_q2_total','team_line_score_q3_total','team_line_score_q4_total','opp_team_id', 'opp_team_conference','opp_team_wins','opp_team_loses','opp_games_played','opp_season_point_total','opp_team_line_score_q1_total','opp_team_line_score_q2_total','opp_team_line_score_q3_total','opp_team_line_score_q4_total']]
+        x = game_data.loc[:,['season', 'week', 'season_type', 'neutral_site', 'conference_game', 'team_id',  'team_conference', 'team_wins','team_loses','games_played','season_point_total','team_line_score_q1_total','team_line_score_q2_total','team_line_score_q3_total','team_line_score_q4_total','opp_team_id', 'opp_team_conference','opp_team_wins','opp_team_loses','opp_games_played','opp_season_point_total','opp_team_line_score_q1_total','opp_team_line_score_q2_total','opp_team_line_score_q3_total','opp_team_line_score_q4_total']]
         y = game_data['team_points'] - game_data['opp_team_points']
+        
+        x.loc[:, 'team_conference'].fillna('N/A', inplace=True)
+        x.loc[:, 'opp_team_conference'].fillna('N/A', inplace=True)
+        
+        x.loc[:, 'team_line_score_q3_total'].fillna(0, inplace=True)
+        x.loc[:, 'opp_team_line_score_q3_total'].fillna(0, inplace=True)
+        
+        x.loc[:, 'team_line_score_q4_total'].fillna(0, inplace=True)
+        x.loc[:, 'opp_team_line_score_q4_total'].fillna(0, inplace=True)
+
 
         # Data Transforms
         x.loc[:, 'season_type'] = LabelEncoder().fit_transform(x['season_type'])
         x.loc[:, 'team_conference'] = LabelEncoder().fit_transform(x['team_conference'])
         x.loc[:, 'opp_team_conference'] = LabelEncoder().fit_transform(x['opp_team_conference'])
-        x.loc[:, 'venue_id'] = LabelEncoder().fit_transform(x['venue_id'])
         x.loc[:, 'team_id'] = LabelEncoder().fit_transform(x['team_id'])
         x.loc[:, 'opp_team_id'] = LabelEncoder().fit_transform(x['opp_team_id'])
 
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.2, random_state=0)
-        regression_model.fit(x_train, y_train)
-        return regression_model.score(x_test, y_test)
+
+        xg_reg = xgb.XGBRegressor(objective='reg:linear',colsample_bytree=0.3, learning_rate=0.1,max_depth=5, alpha=10, n_estimators=10)
+        xg_reg.fit(x_train, y_train)
+        return xg_reg.score(x_test, y_test)
