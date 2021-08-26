9d7b50bbf83d8a37b9f3e6e75b767dfae63938ca
Will Mitchell <ChangedNameTo@users.noreply.github.com>
2021-08-25 20:58:10 -0400

    Fixed stats

---
+++ src/data/team_stats/team_stats_cleaner.py
@@ -0,0 +1,30 @@
+import csv
+import ast
+import json
+import pandas as pd
+
+for year in range(2019,2021):
+    for week in range (1, 17):
+
+        new_stats_file = []
+        outframe = pd.DataFrame()
+
+        with open('src/data/team_stats/{}_week_{}_team_stats.csv'.format(year, week), newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            counter = 0
+            for row in reader:
+                counter += 1
+
+                if counter == 1:
+                    new_stats_file.append(['game_id','team','category','stat'])
+                    continue
+
+                stats_row = row[1]
+                json_stats_row = ast.literal_eval(stats_row)
+
+                for cat in json_stats_row[0]['stats']:
+                    new_stats_file.append([row[0], json_stats_row[0]['school'], cat['category'], cat['stat']])
+
+        with open('src/data/team_stats/{}_week_{}_team_stats_fixed.csv'.format(year, week),'w+', newline='') as outfile:
+            writer = csv.writer(outfile)
+            writer.writerows(new_stats_file)
